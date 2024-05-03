da1a37870cb38c088465f176bb8456057de908c2
Brayden Langley <brayden167@gmail.com>
2024-05-03 16:35:46 -0700

    Remove prompt

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import Prompt from '@babbage/react-prompt'
 import App from './App'
 
 ReactDOM.render(
-  <Prompt
-    customPrompt
-    appName='KeyFunder'
-    author='Peer-to-peer Privacy Systems Research, LLC'
-    authorUrl='https://projectbabbage.com'
-    description='Tools for importing and exporting BSV on the MetaNet'
-  >
-    <App />
-  </Prompt>,
+  <App />,
   document.getElementById('root')
 )
